4378d5017b701b9652d8a561858a962658ea9676
DESKTOP-KR1CIC5\Ari Kujala <ari.kujala@kotiposti.net>
2017-03-14 23:05:32 +0200

    First touch towards multiple js files

---
+++ js/dice/dice.js
@@ -0,0 +1,8 @@
+function d20(){
+  return getRandomInt(1,20);
+}
+
+// Returns a random integer between min (included) and max (included)
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
