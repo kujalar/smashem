e6e870e20ee49bff129de548f58104ee1bd5271f
DESKTOP-KR1CIC5\Ari Kujala <ari.kujala@kotiposti.net>
2017-03-29 19:45:33 +0300

    more monsters

--- js/barrack/barrack.js
+++ js/barrack/barrack.js
@@ -3,6 +3,9 @@ var barrack = {  //namespace barrack
   getNextId: function(){
     return barrack.idSequence++;
   },
+  getOrcWarChief: function() {
+    return barrack.buildMonster(monsterManual.orcWarChief);
+  },
   getOrc: function() {
     var monster = monsterManual.orc;
     return barrack.buildMonster(monster);
@@ -21,9 +24,11 @@ var barrack = {  //namespace barrack
 };
 
 var orcActionList = [{name: 'Greataxe', toHit: 5, hit: '1d12+3', critHit: '1d12'},{name: 'Javelin', toHit: 5, hit: '1d6+3', critHit: '1d12'}];
+var orcWarChiefActionList =   [{name: 'Greataxe', toHit: 6, hit: '1d12+1d8+4', critHit: '1d12+1d8'},{name: 'Spear',toHit: 6, hit: '2d8+4', critHit: '2d8'}];
 var expexterActionList = [{name: 'Sword +2', toHit: 9, hit: '1d8+9', critHit: '1d8'}];
 
 var monsterManual = {
   orc: {name: 'Orc', avatar: 'pic/tuntematon.png',rollHp: '2d8+6',ac: 13,nOfAttacks: 1,attackList: orcActionList},
+  orcWarChief: {name: 'Orc War Chief', avatar: 'pic/tuntematon.png', rollHp: '11d8+44',ac: 16, nOfAttacks: 2, attackList: orcWarChiefActionList},
   expexter: {name: 'Expexter', avatar: 'pic/platewarrior.png',rollHp: '55',ac: 22, nOfAttacks: 2,attackList: expexterActionList }
 }
